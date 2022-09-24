270631343dd611c0412eaae66af2e862cb358e6b
HarryPotter <1208314139@qq.com>
2022-09-24 16:04:09 +0800

    add cmd interface

--- courses/nlp_course/compute_entropy.py
+++ courses/nlp_course/compute_entropy.py
@@ -4,6 +4,9 @@ from math import log, e
 import re
 from tqdm import tqdm
 import jieba
+import pandas as pd
+from nltk import word_tokenize
+import argparse
 
 class Vocabulary():
     def __init__(self):
@@ -19,8 +22,9 @@ class Vocabulary():
         self.label_set.add(label)
 
     def add_sequence(self, labels):
-        for label in labels:
-            self.add(label)
+        # for label in labels:
+        #     self.add(label)
+        self.label_set = self.label_set.union(set(labels))
 
     def add_dict(self, defined_label2id_dict):
         # TODO:Resolve dictionary key value pair conflicts situation
@@ -70,88 +74,163 @@ class Vocabulary():
     def get_id2label_dict(self):
         return self.id2label_dict
 
-def entropy2(labels, base=None):
-  """ Computes entropy of label distribution. """
+def cal_entropy(labels, base=None):
+    """ Computes entropy of label distribution. """
 
-  n_labels = len(labels)
+    n_labels = len(labels)
 
-  if n_labels <= 1:
-    return 0
+    if n_labels <= 1:
+        return 0
 
-  value,counts = np.unique(labels, return_counts=True)
-  probs = counts / n_labels
-  n_classes = np.count_nonzero(probs)
+    value,counts = np.unique(labels, return_counts=True)
+    probs = counts / n_labels
+    n_classes = np.count_nonzero(probs)
 
-  if n_classes <= 1:
-    return 0
+    if n_classes <= 1:
+        return 0
 
-  ent = 0.
+    ent = 0.
 
-  # Compute entropy
-  base = e if base is None else base
-  for i in probs:
-    ent -= i * log(i, base)
+    # Compute entropy
+    base = e if base is None else base
+    for i in probs:
+        ent -= i * log(i, base)
 
-  return ent
+    return ent
 
 
-# novel_path = "./new_novels"
-# novel_files = os.listdir(novel_path)
-# all_words = []
-# vocab = Vocabulary()
-# for novel_file in tqdm(novel_files):
-#     with open(os.path.join(novel_path, novel_file), "r", encoding='UTF-8') as f:
-#         lines = f.readlines()
-#         processed_lines = [line.rstrip() for line in lines]
-#         for line in processed_lines:
-#             line = re.sub('[^\u4e00-\u9fa5]+','',line)
-#             all_words.extend(line)
-#             vocab.add_sequence(line)
-#
-# print("{} words detected.".format(len(all_words)))
-# print("Creating vocab...")
-# vocab.create()
-# print("Calculating entropy...")
-# all_word_ids = list(map(vocab.label2id_dict.get,all_words))
-# entropy_result = entropy2(all_word_ids)
-# print(entropy_result)
+def read_baike(baike_file,mode="character",debug=False):
+    if mode not in ["character","word"]:
+        raise ValueError("Mode {} is not supported!".format(mode))
+    df = pd.read_excel(baike_file, index_col=0)
+    all_words = []
+    for idx, row in tqdm(df.iterrows(), total=df.shape[0]):
+        if debug:
+            if idx >= 500:
+                break
+        text_piece = row["text"]
+        if (isinstance(text_piece, str)):
+            text_piece = re.sub('[^\u4e00-\u9fa5]+', '', text_piece)
+            if mode == "word":
+                seg_generator = jieba.cut("".join(text_piece), use_paddle=True)
+                seg_list = list(seg_generator)
+                all_words.extend(seg_list)
+            if mode == "character":
+                character_list = list(text_piece)
+                all_words.extend(character_list)
+    return all_words
 
-import pandas as pd
-baike_file = r"baike/data-baike.xlsx"
-df = pd.read_excel(baike_file,index_col=0)
-all_words = []
-vocab = Vocabulary()
+def read_novels(novel_path,mode="character",debug=False):
+    if mode not in ["character","word"]:
+        raise ValueError("Mode {} is not supported!".format(mode))
+    novel_files = os.listdir(novel_path)
+    all_words = []
+    for novel_file in tqdm(novel_files):
+        with open(os.path.join(novel_path, novel_file), "r", encoding='UTF-8') as f:
+            lines = f.readlines()
+            processed_lines = [line.rstrip() for line in lines]
+            for line in processed_lines:
+                text_piece = re.sub('[^\u4e00-\u9fa5]+','',line)
+                if mode == "word":
+                    seg_generator = jieba.cut("".join(text_piece), use_paddle=True)
+                    seg_list = list(seg_generator)
+                    all_words.extend(seg_list)
+                if mode == "character":
+                    character_list = list(text_piece)
+                    all_words.extend(character_list)
+        if debug:
+            break
+    return all_words
 
-novel_path = "./new_novels"
-novel_files = os.listdir(novel_path)
-for novel_file in tqdm(novel_files):
-    with open(os.path.join(novel_path, novel_file), "r", encoding='UTF-8') as f:
-        lines = f.readlines()
-        processed_lines = [line.rstrip() for line in lines]
-        for line in processed_lines:
-            line = re.sub('[^\u4e00-\u9fa5]+','',line)
-            seg_generator = jieba.cut("".join(line),use_paddle=True)
-            seg_list = list(seg_generator)
-            all_words.extend(seg_list)
-            vocab.add_sequence(seg_list)
+def read_english_novels(novel_path,mode="character",debug=False):
+    if mode not in ["character","word"]:
+        raise ValueError("Mode {} is not supported!".format(mode))
+    novel_files = os.listdir(novel_path)
+    all_words = []
+    for novel_file in tqdm(novel_files):
+        with open(os.path.join(novel_path, novel_file), "r", encoding='UTF-8') as f:
+            lines = f.readlines()
+            processed_lines = [line.rstrip() for line in lines]
+            for line in processed_lines:
+                if mode == 'word':
+                    text_piece = word_tokenize(line)
+                    result = list(filter(lambda ele: re.search("[a-zA-Z\s]+", ele) is not None, text_piece))
+                    result = list(map(lambda x: x.lower(),result))
+                    all_words.extend(list(result))
+                if mode == 'character':
+                    text_piece = re.sub('[^a-zA-Z]+','',line)
+                    all_words.extend(list(text_piece))
+        if debug:
+            break
+    return all_words
 
-for idx,row in tqdm(df.iterrows(),total=df.shape[0]):
-    # if idx >= 500:
-    #     break
-    text_piece = row["text"]
-    if(isinstance(text_piece,str)):
-        text_piece = re.sub('[^\u4e00-\u9fa5]+', '', text_piece)
-        seg_generator = jieba.cut("".join(text_piece),use_paddle=True)
-        # print("/".join(seg_list))
-        seg_list = list(seg_generator)
-        all_words.extend(seg_list)
-        vocab.add_sequence(seg_list)
-    # break
+def cal_entropy_of_words(words_list):
+    vocab = Vocabulary()
+    vocab.add_sequence(words_list)
+    vocab.create()
+    all_word_ids = list(map(vocab.label2id_dict.get,words_list))
+    entropy_result = cal_entropy(all_word_ids)
+    return entropy_result
 
-print("{} words detected.".format(len(all_words)))
-print("Creating vocab...")
-vocab.create()
-print("Calculating entropy...")
-all_word_ids = list(map(vocab.label2id_dict.get,all_words))
-entropy_result = entropy2(all_word_ids)
-print(entropy_result)
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Entropy computing parameters')
+
+    parser.add_argument("-l",'--language',type=str,help='language can be chinese or english',default='english')
+    parser.add_argument('-d','--datapath', type=str, help='data source', required=True)
+    parser.add_argument('-m','--mode',type=str,help='mode can be character or word',default='character',)
+    parser.add_argument('-p','--percentage',type=float,help="the proportion of all the texts used",default=1)
+    args = vars(parser.parse_args())
+    print(args)
+
+    language = args["language"]
+    datapath = args["datapath"]
+    mode = args["mode"]
+    percentage = args["percentage"]
+    if language not in ["english","chinese"]:
+        raise ValueError("Language {} is not supported!".format(language))
+    if mode not in ["character","word"]:
+        raise ValueError("Mode {} is not supported!".format(mode))
+    assert percentage <= 1 and percentage >= 0
+
+    mapping = {"chinese":read_novels,"english":read_english_novels}
+
+    print("Reading files for language {} in mode {}...".format(language,mode))
+    func = mapping[language]
+    all_words = func(datapath,mode)
+    print("Calculating entropy for language {} in mode {}...".format(language,mode))
+    some_words = all_words[:int(percentage * len(all_words))]
+    entropy = cal_entropy(some_words)
+    print("For language {} in mode {} using percentage {} the entropy is {}".format(language,mode,percentage,entropy))
+
+
+    # baike_file = "baike/data-baike.xlsx"
+    # novel_path = "new_novels"
+    # english_novel_path = "english_novels"
+    #
+    # print("Reading files for english words entropy...")
+    # all_english_letters = read_english_novels(english_novel_path,mode='word',debug=False)
+    # print("Calculating english words entropy...")
+    # english_letter_entropy = cal_entropy_of_words(all_english_letters)
+    # print("English words entropy:",english_letter_entropy)
+    #
+    # print("Reading files for english letter entropy...")
+    # all_english_letters = read_english_novels(english_novel_path,mode='character',debug=False)
+    # print("Calculating english letter entropy...")
+    # english_letter_entropy = cal_entropy_of_words(all_english_letters)
+    # print("English letter entropy:",english_letter_entropy)
+    #
+    # # character entropy
+    # print("Reading files for character entropy...")
+    # all_baike_characters = read_baike(baike_file,mode="character",debug=False)
+    # all_novel_characters = read_novels(novel_path,mode="character",debug=False)
+    # print("Calculating character entropy...")
+    # character_entropy = cal_entropy_of_words(all_novel_characters + all_baike_characters)
+    # print("Chinese character entropy:", character_entropy)
+    #
+    # # word entropy
+    # print("Reading files for word entropy...")
+    # all_baike_words= read_baike(baike_file,mode="word",debug=False)
+    # all_novel_words = read_novels(novel_path,mode="word",debug=False)
+    # print("Calculating word entropy...")
+    # word_entropy = cal_entropy_of_words(all_novel_words + all_baike_words)
+    # print("Chinese word entropy:",word_entropy)
